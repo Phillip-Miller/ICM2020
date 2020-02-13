15229fb28d5dcf0e25363af9c7e86a3c34169867
Phillip M <phillipmiller@sandiego.edu>
2020-02-13 15:18:53 -0800

    setting up file system

---
+++ Linkfinders.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 13 15:13:46 2020
+
+@author: PhillipM
+"""
+
+import matplotlib.pyplot as pl
+import math
+import random
+import numpy
+import pandas
+import statistics
+import scipy
+
