6bc1cf10722cb38cd18f989a13627d1c446c2293
Phillip M <phillipmiller@sandiego.edu>
2020-02-13 23:29:43 -0800

    began to make links and nodes

--- Linkfinders.py
+++ Linkfinders.py
@@ -8,14 +8,20 @@ Created on Thu Feb 13 15:13:46 2020
 import matplotlib.pyplot as plt
 import math
 import random
-import numpy
+import numpy as np
 import statistics
 import scipy
+import networkx as nx
+import pandas as pd
 fullevents = open(r"2020_Problem_D_DATA\fullevents.csv", "r")
 matches = open(r"2020_Problem_D_DATA\matches.csv", "r")
 passingevents = open(r"2020_Problem_D_DATA\passingevents.csv", "r")
 coachWinLoss = {"Coach1":0,"Coach2":0,"Coach3":0}
+"""
+Plot heatmap of where players spend time
 
+Do a whole bunch of plots, and stick code and graph in google docs
+"""
 class Player:
     def __init__(self,):
         """
@@ -27,6 +33,13 @@ class triangle(Player):
 
 class analyzeData:
     def mainM():
+        data = pd.read_csv(r"2020_Problem_D_DATA\passingevents.csv")
+        print(data.head())
+        
+        df = nx.from_pandas_edgelist(data, source='OriginPlayerID', target='DestinationPlayerID', edge_attr=True)
+        plt.figure(figsize=(50,50))
+        nx.draw_networkx(df, with_labels=True)
+    def plotCoaches():
         global coachWinLoss
         for line in matches:
             tokens = line.strip().split(",")
